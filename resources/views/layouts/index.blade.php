d1474f9baa8a30ce3e21754fad2227b5855c7208
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-08-05 02:19:20 +0200

    add point to href

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="/user/css/main.css">
-    <link rel="stylesheet" href="/user/css/fixed-nav.css">
-    <link rel="stylesheet" href="/user/css/header.css">
-    <link rel="stylesheet" href="/user/css/fonts.css">
-    <link rel="stylesheet" href="/user/css/icons.css">
-    <link rel="stylesheet" href="/user/css/section-title.css">
+    <link rel="stylesheet" href="./user/css/main.css">
+    <link rel="stylesheet" href="./user/css/fixed-nav.css">
+    <link rel="stylesheet" href="./user/css/header.css">
+    <link rel="stylesheet" href="./user/css/fonts.css">
+    <link rel="stylesheet" href="./user/css/icons.css">
+    <link rel="stylesheet" href="./user/css/section-title.css">
 
     <title>@yield('title')</title>
 </head>
@@ -19,8 +19,8 @@
 
     @yield('content')
 
-    <script src="/user/js/fixed-nav.js"></script>
-    <script src="/user/js/photos-scroll.js"></script>
+    <script src="./user/js/fixed-nav.js"></script>
+    <script src="./user/js/photos-scroll.js"></script>
 </body>
 
 </html>
