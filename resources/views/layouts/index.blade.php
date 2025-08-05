16121158ffb780629c854bd29365f3446c946908
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-08-05 02:40:55 +0200

    add route for assets

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="{{ asset('public/user/css/main.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/user/css/fixed-nav.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/user/css/header.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/user/css/fonts.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/user/css/icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('public/user/css/section-title.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/css/fixed-nav.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/css/header.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/css/fonts.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/css/icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/css/section-title.css') }}">
 
     <title>@yield('title')</title>
 </head>
@@ -19,8 +19,8 @@
 
     @yield('content')
 
-    <script src="{{ asset('public/user/js/fixed-nav.js') }}"></script>
-    <script src="{{ asset('public/user/js/photos-scroll.js') }}"></script>
+    <script src="{{ asset('user/js/fixed-nav.js') }}"></script>
+    <script src="{{ asset('user/js/photos-scroll.js') }}"></script>
 </body>
 
 </html>
