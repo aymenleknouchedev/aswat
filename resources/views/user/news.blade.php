037e12c9dbeec4a09e6da1a596104f32b49af9dc
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-10-06 12:46:20 +0200

    newspage

--- resources/views/user/news.blade.php
+++ resources/views/user/news.blade.php
@@ -111,7 +111,7 @@
 
         /* ===================== CONTENT ===================== */
         .custom-article-content {
-            font-size: 18px;
+            font-size: 16px !important;
             font-family: asswat-regular;
             color: #333;
             line-height: 1.9;
@@ -119,8 +119,8 @@
             margin-bottom: 30px;
         }
 
-        .custom-article-content span {
-            font-size: 18px;
+        .custom-article-content p span {
+            font-size: 16px !important;
             font-family: asswat-regular;
             color: #333;
             line-height: 1.9;
