ff3248c6fe7aa257d23fb104169268856784f657
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-10-26 23:54:47 +0100

    push

--- resources/views/user/latest-news.blade.php
+++ resources/views/user/latest-news.blade.php
@@ -117,27 +117,9 @@
                     @forelse ($latestContents as $item)
                         <div class="newCategory-all-card">
                             <div class="newCategory-all-card-date">
-                                @php
-                                    $months = [
-                                        '01' => 'يناير',
-                                        '02' => 'فبراير',
-                                        '03' => 'مارس',
-                                        '04' => 'أبريل',
-                                        '05' => 'مايو',
-                                        '06' => 'يونيو',
-                                        '07' => 'يوليو',
-                                        '08' => 'أغسطس',
-                                        '09' => 'سبتمبر',
-                                        '10' => 'أكتوبر',
-                                        '11' => 'نوفمبر',
-                                        '12' => 'ديسمبر',
-                                    ];
-                                    $date = $item->created_at;
-                                    $day = $date->format('d');
-                                    $month = $months[$date->format('m')];
-                                    $year = $date->format('Y');
-                                @endphp
-                                <h4>{{ $day }} {{ $month }} {{ $year }}</h4>
+                                <h4>
+                                    {{ \Carbon\Carbon::parse($item->created_at)->diffForHumans() }}
+                                </h4>
                             </div>
                             <div class="newCategory-all-card-image">
                                 <img src="{{ $item->media()->wherePivot('type', 'main')->first()->path ?? './user/assets/images/IMG20.jpg' }}"
