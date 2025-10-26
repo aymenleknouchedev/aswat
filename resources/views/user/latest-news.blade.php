19ff17a2264c1f4ccd364c02f4d89eb568512351
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-10-27 00:22:48 +0100

    push

--- resources/views/user/latest-news.blade.php
+++ resources/views/user/latest-news.blade.php
@@ -117,7 +117,7 @@
                     @forelse ($latestContents as $item)
                         <div class="newCategory-all-card">
                             <div class="newCategory-all-card-date">
-                                <h4 style="width: 130px">
+                                <h4 style="width: 100px">
                                     @php
                                         \Carbon\Carbon::setLocale('ar'); // تفعيل اللغة العربية
 
