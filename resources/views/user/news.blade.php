31c5172ac38edaa8e403b3a478318a9433cc7d11
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-10-26 03:28:10 +0100

    push

--- resources/views/user/news.blade.php
+++ resources/views/user/news.blade.php
@@ -70,7 +70,7 @@
         }
 
         .custom-article-title {
-            font-size: 40px;
+            font-size: 45px;
             font-family: asswat-regular;
             color: #141414;
             line-height: 1.4;
