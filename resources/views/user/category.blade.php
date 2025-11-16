a60e6b593cae81d64a4980754354228a93f19369
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-11-16 23:52:18 +0100

    push

--- resources/views/user/category.blade.php
+++ resources/views/user/category.blade.php
@@ -49,18 +49,17 @@
         .newCategory-all-card-text h3 {
             font-family: asswat-regular !important;
             font-weight: normal !important;
-            margin-top: 8px !important;
             font-size: 16px;
             color: #7c7c74;
             margin: 0 0 4px 0;
         }
 
-        /* .newCategory-all-card-text h2 {
+        .newCategory-all-card-text h2 {
             font-family: asswat-bold;
             font-size: 18px;
             margin: 0 0 8px 0;
             line-height: 1.4;
-        } */
+        }
 
         .newCategory-all-card-text p {
             font-size: 15px;
