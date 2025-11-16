fbfcf392a8b0236b9bd2d77a1464a4428affbe2c
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-11-17 00:55:48 +0100

    push

--- resources/views/user/latest-news.blade.php
+++ resources/views/user/latest-news.blade.php
@@ -70,6 +70,7 @@
         .newCategory-all-card-text p {
             font-size: 16px !important;
             line-height: 1.5 !important;
+            color: #555 !important;
             margin: 0 !important;
         }
 
