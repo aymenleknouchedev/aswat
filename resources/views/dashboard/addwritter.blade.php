5271ad9f6a33b4c392136df6069cca73aa79b6db
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-11-17 22:53:14 +0100

    showwithphoto

--- resources/views/dashboard/addwritter.blade.php
+++ resources/views/dashboard/addwritter.blade.php
@@ -207,7 +207,7 @@
                             <div class="form-group">
                                 <label class="form-label" for="bio" data-en="Bio" data-ar="نبذة">نبذة</label>
                                 <div class="form-control-wrap">
-                                    <textarea name="bio" class="form-control" id="bio" rows="4" maxlength="200" required>{{ old('bio', isset($writer) ? $writer->bio : '') }}</textarea>
+                                    <textarea name="bio" class="form-control" id="bio" rows="4" maxlength="200">{{ old('bio', isset($writer) ? $writer->bio : '') }}</textarea>
                                     <small class="text-muted">
                                         <span
                                             id="bio-count">{{ strlen(old('bio', isset($writer) ? $writer->bio : '')) }}</span>
