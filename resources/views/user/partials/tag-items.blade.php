e24f82452b035451af723a978352eb4765dfc99f
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-11-18 16:06:32 +0100

    test

--- resources/views/user/partials/tag-items.blade.php
+++ resources/views/user/partials/tag-items.blade.php
@@ -10,6 +10,7 @@
 
         <!-- Text -->
         <div class="newCategory-all-card-text">
+            <h3>{{ $item->category->name ?? '' }}</h3>
             <a href="{{ route('news.show', $item->title) }}"
                 style="text-decoration: none; color: inherit;">
                 <h2>{{ $item->title }}</h2>
