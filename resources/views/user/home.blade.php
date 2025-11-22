a1d9e1a825550f2e59a63e9e8349ae1d6bc4ed8d
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-11-22 02:37:49 +0100

    post

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -417,7 +417,7 @@
                 left: 0;
                 right: 0;
                 bottom: 0;
-                background: linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.0));
+                background: linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.0));
                 z-index: 1;
             }
 
