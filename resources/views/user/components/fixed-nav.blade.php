d4c9c3e331e7502a429e155cc4a20c7eaa497f75
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-12-07 15:58:27 +0100

    searchproblemui

--- resources/views/user/components/fixed-nav.blade.php
+++ resources/views/user/components/fixed-nav.blade.php
@@ -152,6 +152,11 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
         <style>
+
+            .site-search-form{
+                margin: 0px;
+            }
+
             .admin-top-bar {
                 position: fixed;
                 top: 0;
