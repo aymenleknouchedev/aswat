8b6b0158052f7a9c3ce4c0c8417a932dc8b095b1
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-12-17 00:28:53 +0100

    deleteroutes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\JoinTeamController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 
 require __DIR__ ."/mail.php";
 require __DIR__ ."/client.php";
@@ -11,22 +10,7 @@ require __DIR__ ."/admin.php";
 // Coming soon
 Route::post('/store-join-team', [JoinTeamController::class, 'store_join_team'])->name('dashboard.store-join-team');
 
-// Clear cache, config, routes, views
-Route::get('/clear-cache', function () {
-    Artisan::call('cache:clear');
-    return 'Cache, config, routes, and views cleared successfully.';
-});
 
-// Route to run migrate:fresh
-Route::get('/migrate-fresh', function () {
-    Artisan::call('migrate:fresh', ['--force' => true]);
-    return response('Database migrated fresh successfully.', 200);
-});
-
-Route::get('/storage-link', function () {
-    Artisan::call('storage:link');
-    return response('The [public/storage] directory has been re-linked.', 200);
-});
 
 // Debug weather detection
 Route::get('/debug/weather', function () {
@@ -46,9 +30,3 @@ Route::get('/debug/weather', function () {
     ]);
 });
 
-Route::get('/seed', function () {
-    Artisan::call('db:seed', [
-        '--force' => true
-    ]);
-    return 'Database seeded successfully.';
-});
