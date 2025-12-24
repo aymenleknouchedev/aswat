55ef78b4232807290696851f74eb55a2317e276f
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-12-24 19:44:43 +0100

    push

--- resources/views/user/latest-news.blade.php
+++ resources/views/user/latest-news.blade.php
@@ -208,26 +208,7 @@
                     @forelse ($latestContents as $item)
                         <div class="newCategory-all-card">
                             <div class="newCategory-all-card-date">
-                                <h4 style="width: 140px">
-                                    @php
-                                        \Carbon\Carbon::setLocale('ar'); // تفعيل اللغة العربية
-
-                                        $created = \Carbon\Carbon::parse($item->published_at);
-                                        $now = \Carbon\Carbon::now();
-                                        $diffHours = $created->diffInHours($now);
-                                    @endphp
-
-                                    @if ($diffHours < 24)
-                                        <div style="display: flex; flex-direction: column; align-items: flex-start;">
-                                            <span>{{ $created->diffForHumans(null, null, false, 1) }}</span>
-                                        </div>
-                                    @else
-                                        <div style="display: flex; flex-direction: column; align-items: flex-start;">
-                                            <span>{{ $created->translatedFormat('d F Y') }}</span>
-                                            <span style="color: #74747C;">{{ $created->translatedFormat('H:i') }}</span>
-                                        </div>
-                                    @endif
-                                </h4>
+                                
                             </div>
                             <div class="newCategory-all-card-image">
                                 <a href="{{ route('news.show', $item->shortlink) }}">
