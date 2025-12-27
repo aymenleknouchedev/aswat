2487aa7ae91af82c9cf9526d262220566147f1b7
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-12-27 03:52:55 +0100

    push

--- resources/views/user/components/world.blade.php
+++ resources/views/user/components/world.blade.php
@@ -13,12 +13,15 @@
 
     .world-feature {
         position: relative;
+        background-color: #f5f5f5;
+        padding: 20px;
+        border-radius: 8px;
     }
 
     .world-feature img {
         width: 100%;
         aspect-ratio: 16 / 9;
-
+        border-radius: 4px;
     }
 
     .world-feature h2 {
