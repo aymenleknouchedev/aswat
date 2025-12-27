6d5501890ba7fdb735fd691914b341cc5a691706
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2025-12-27 04:55:42 +0100

    push

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -235,7 +235,7 @@
                 display: inline-block;
                 color: #fff;
                 text-decoration: none;
-                font-size: 20px;
+                font-size: 1rem;
                 font-weight: 800;
                 line-height: 1.4;
                 font-family: 'asswat-bold';
