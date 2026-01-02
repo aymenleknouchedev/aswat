fdb56791c23486aea96e01d79aeda9bedaa9b343
aymenleknouchedev <leknoucheaymen2025@gmail.com>
2026-01-02 21:55:23 +0100

    headerorder

--- resources/views/user/components/header.blade.php
+++ resources/views/user/components/header.blade.php
@@ -222,25 +222,25 @@
                 </a>
             </div>
             <div class="news-item">
-                <a href="{{ route('news.show', $topContents[4]->content->shortlink) }}">
-                    <img src="{{ $topContents[4]->content->media()->wherePivot('type', 'main')->first()->path ?? '' }}"
+                <a href="{{ route('news.show', $topContents[5]->content->shortlink) }}">
+                    <img src="{{ $topContents[5]->content->media()->wherePivot('type', 'main')->first()->path ?? '' }}"
                         alt="News 2">
                 </a>
                 <h3>
-                    <x-category-links :content="$topContents[4]->content" />
+                    <x-category-links :content="$topContents[5]->content" />
                 </h3>
-                <a href="{{ route('news.show', $topContents[4]->content->shortlink) }}"
+                <a href="{{ route('news.show', $topContents[5]->content->shortlink) }}"
                     style="text-decoration: none; color: inherit;">
-                    <p>{{ $topContents[4]->content->title ?? '' }}</p>
+                    <p>{{ $topContents[5]->content->title ?? '' }}</p>
                 </a>
             </div>
             <div class="news-item-noimage">
                 <h3>
-                    <x-category-links :content="$topContents[7]->content" />
+                    <x-category-links :content="$topContents[6]->content" />
                 </h3>
-                <a href="{{ route('news.show', $topContents[7]->content->shortlink) }}"
+                <a href="{{ route('news.show', $topContents[6]->content->shortlink) }}"
                     style="text-decoration: none; color: inherit;">
-                    <p>{{ $topContents[7]->content->title ?? '' }}</p>
+                    <p>{{ $topContents[6]->content->title ?? '' }}</p>
                 </a>
             </div>
         </div>
