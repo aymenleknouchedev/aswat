30c0f4810d82d821a1e8bc366d1ee95b1e2843d0
MAC <mac@MACs-MacBook-Pro.local>
2026-06-10 09:27:53 +0100

    push

--- resources/views/components/category-links.blade.php
+++ resources/views/components/category-links.blade.php
@@ -1,21 +1,25 @@
+<style>
+    .category-link-hover { text-decoration: none; }
+    .category-link-hover:hover { text-decoration: underline; }
+</style>
 @if ($content->category && $content->country)
-    <a href="{{ route('category.show', ['id' => $content->category->id, 'type' => 'Category']) }}">
+    <a href="{{ route('category.show', ['id' => $content->category->id, 'type' => 'Category']) }}" class="category-link-hover">
         {{ $content->category->name ?? '' }}
     </a>
     -
-    <a href="{{ route('category.show', ['id' => $content->country->id, 'type' => 'Country']) }}">
+    <a href="{{ route('category.show', ['id' => $content->country->id, 'type' => 'Country']) }}" class="category-link-hover">
         {{ $content->country->name ?? '' }}
     </a>
 @elseif ($content->category && $content->continent)
-    <a href="{{ route('category.show', ['id' => $content->category->id, 'type' => 'Category']) }}">
+    <a href="{{ route('category.show', ['id' => $content->category->id, 'type' => 'Category']) }}" class="category-link-hover">
         {{ $content->category->name ?? '' }}
     </a>
     -
-    <a href="{{ route('category.show', ['id' => $content->continent->id, 'type' => 'Continent']) }}">
+    <a href="{{ route('category.show', ['id' => $content->continent->id, 'type' => 'Continent']) }}" class="category-link-hover">
         {{ $content->continent->name ?? '' }}
     </a>
 @elseif ($content->category)
-    <a href="{{ route('category.show', ['id' => $content->category->id, 'type' => 'Category']) }}">
+    <a href="{{ route('category.show', ['id' => $content->category->id, 'type' => 'Category']) }}" class="category-link-hover">
         {{ $content->category->name ?? '' }}
     </a>
 @endif
